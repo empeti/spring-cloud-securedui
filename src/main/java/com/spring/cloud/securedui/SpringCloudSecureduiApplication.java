f1e1ed4ad2b6cc8d9d96faf2b3850af9ce8ae765
empet <mpeti@mpeti.hu>
2017-06-18 16:06:14 +0200

    Spring bootstrap starter project init

---
+++ src/main/java/com/spring/cloud/securedui/SpringCloudSecureduiApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.securedui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSecureduiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSecureduiApplication.class, args);
+	}
+}
